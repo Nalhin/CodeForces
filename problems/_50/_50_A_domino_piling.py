dac2baa740c87dec4f21d48eeff3f310114cac08
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-11-03 19:43:46 +0100

    feat: Domino Pilling (50 A) solution.

---
+++ problems/_50/_50_A_domino_piling.py
@@ -0,0 +1,12 @@
+"""
+    Submission - https://codeforces.com/contest/50/submission/134246818
+"""
+
+
+def main():
+    n, m = (int(i) for i in input().split())
+    print(n * m // 2)
+
+
+if __name__ == '__main__':
+    main()
