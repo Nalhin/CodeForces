d1bd0cf7e5429642d6bc229390feb6587922bda9
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-11-03 22:58:04 +0100

    feat: Elephant (617 A) solution.

---
+++ problems/_617/A/_617_A_elephant.py
@@ -0,0 +1,13 @@
+"""
+    Submission - https://codeforces.com/contest/617/submission/134258508
+"""
+from math import ceil
+
+
+def main() -> None:
+    n = int(input())
+    print(ceil(n / 5))
+
+
+if __name__ == '__main__':
+    main()
