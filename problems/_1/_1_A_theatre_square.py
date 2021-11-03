b04dcf7b0ae30cb9fbad41c29329ab5e97fc1080
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-11-03 20:17:56 +0100

    feat: Theatre Square (1 A) solution.

---
+++ problems/_1/_1_A_theatre_square.py
@@ -0,0 +1,14 @@
+"""
+    Submission - https://codeforces.com/contest/1/submission/134249306
+"""
+from math import ceil
+
+
+def main():
+    n, m, a = [int(i) for i in input().split()]
+
+    print(ceil(n / a) * ceil(m / a))
+
+
+if __name__ == '__main__':
+    main()
